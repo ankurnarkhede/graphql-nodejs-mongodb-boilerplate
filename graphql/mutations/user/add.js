874ad3956317126c5c2158ebef7f9817b59deb0b
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-11-27 18:36:05 +0530

    till user mutations

---
+++ graphql/mutations/user/add.js
@@ -0,0 +1,26 @@
+
+
+import {
+    GraphQLNonNull
+} from "graphql";
+
+import {userType, userInputType} from "../../types/user";
+import UserModel from '../../../models/user';
+
+export default {
+    type: userType,
+    args:{
+        data:{
+            name: 'data',
+            type: new GraphQLNonNull(userInputType)
+        }
+    },
+    resolve(root,params){
+        const uModel=new UserModel(params.data);
+        const newUser=uModel.save();
+        if(!newUser){
+            throw new Error('Error adding User..!');
+        }
+        return newUser;
+    }
+}
