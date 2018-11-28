100c2f2b252318f4b8d6df33342bae5e78fbc0c4
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-11-28 11:25:09 +0530

    removed index.js from types...imported from direct files...

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
-
-
 import express from 'express';
 import mongoose from 'mongoose';
 import graphqlHTTP from 'express-graphql';
 
 import schema from './graphql';
 
-const app=express();
+const app = express();
 
 // mongoose connection
 mongoose.connect('mongodb://ankur:ankur1@ds239309.mlab.com:39309/nodejs-graphql-mongodb-boilerplate');
 
-const db=mongoose.connection;
-db.on('error',()=>console.log("Failed to connect to MongoDB"))
-    .once('open',()=>console.log('Connected to MongoDB'))
-
+const db = mongoose.connection;
+db.on('error', () => console.log("Failed to connect to MongoDB"))
+    .once('open', () => console.log('Connected to MongoDB'))
 
 // registering GET / route
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello World! This is GraphQL API");
-
 });
 
-
 // grapgql api endpoint
-
-app.use('/graphql',graphqlHTTP(()=>({
+app.use('/graphql', graphqlHTTP(() => ({
     schema,
-    graphql: true,
+    graphiql: true,
     pretty: true
 })));
 
 
 // server start
-app.listen(3000,()=>{
-    console.log("graphQL API running at port 3000\nLink: http://localhost:3000");
+app.listen(3000, () => {
+    console.log("graphQL API running at port 3000\nLink: http://localhost:3000/graphql");
 
 });
 
