16e0cc253e564022ae331ac6e90cd62eec41ddb4
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-03-05 16:43:29 +0530

    changed deprecated stuff

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import graphqlHTTP from 'express-graphql'
-
 import schema from './graphql'
 
 const { SubscriptionServer } = require('subscriptions-transport-ws')
@@ -13,8 +12,9 @@ const app = express()
 
 // mongoose connection
 mongoose.connect(
-  'mongodb://ankur:ankur1@ds239309.mlab.com:39309/nodejs-graphql-mongodb-boilerplate')
-// mongoose.connect('mongodb://localhost:27017/nodejs-graphql-mongodb-boilerplate');
+  'mongodb://ankur:ankur1@ds239309.mlab.com:39309/nodejs-graphql-mongodb-boilerplate',
+  { useNewUrlParser: true })
+mongoose.set('useCreateIndex', true)
 
 const db = mongoose.connection
 db.on('error', () => console.log('Failed to connect to MongoDB')).
